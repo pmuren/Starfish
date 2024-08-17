fae49dd3e5aa1c4c6753c5ac79ac5cbe5cb316d7
Paul Muren <paulmuren@gmail.com>
2024-08-17 00:39:07 -0400

    uhhh, our protagonist is good at pushing, bad at moving :)

--- protag.gd
+++ protag.gd
@@ -9,13 +9,14 @@ func _ready():
 	bicep = $Bicep0
 	if bicep:
 		initial_rest_length = bicep.rest_length
-		reaching_rest_length = 300
-		restitution_dampening = 10
-
+		reaching_rest_length = 80
+		restitution_dampening = 15
 
 func _input(event):
 	if event.is_action_pressed("reach"):
 		if bicep:
+			var direction_to_arm = ($LowerArm0.global_position - self.global_position).normalized();
+			$LowerArm0.apply_central_force(direction_to_arm)
 			bicep.rest_length = reaching_rest_length
 		else:
 			print("Bicep0 not found!")
